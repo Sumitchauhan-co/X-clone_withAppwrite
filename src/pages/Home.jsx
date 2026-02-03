db5467042fcf64693184e96cae541350f22a28ce
Sumitchauhan-co <chauhan.sumit3012@gmail.com>
2026-02-03 14:02:35 +0530

    v1.1.2.1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -42,13 +42,13 @@ const Home = () => {
         <div className="h-6 w-6 border rounded-[50%] grid place-content-center bg-[#84c346] text-[#eaeaea] text-sm absolute top-3 left-3 cursor-pointer">
           <span>{initial}</span>
         </div>
-        <div className="h-fit w-50 invisible group-focus-within:visible group-active-within:visible fixed z-2 left-3 bottom-12 flex flex-col justify-center rounded-2xl transition-all duration-700 ease-out transform translate-y-0 text-white text-[0.8rem] sm:text-sm font-bold bg-black shadow-white shadow-[0_0_7px_rgba(0,0,0,0.05)] animate-pulse">
-          <div className="h-7 pl-5 mt-3 w-full flex justify-start items-center hover:bg-(--bg-primary-color) active:bg-(--bg-primary-color)">
+        <div className="h-fit w-65 invisible group-focus-within:visible group-active-within:visible fixed z-2 left-3 bottom-12 flex flex-col justify-center rounded-2xl transition-all duration-700 ease-out transform translate-y-0 text-white sm:text-sm font-bold bg-black shadow-white shadow-[0_0_7px_rgba(0,0,0,0.05)] animate-pulse">
+          <div className="h-10 pl-5 mt-3 w-full flex justify-start items-center hover:bg-(--bg-primary-color) active:bg-(--bg-primary-color)">
             <span>Add an existing account</span>
           </div>
           <div
             onClick={handleLogout}
-            className="h-7 pl-5 mb-3 w-full flex justify-start items-center hover:bg-(--bg-primary-color) active:bg-(--bg-primary-color)"
+            className="h-10 pl-5 mb-3 w-full flex justify-start items-center hover:bg-(--bg-primary-color) active:bg-(--bg-primary-color)"
           >
             <span>Log out</span>
           </div>
